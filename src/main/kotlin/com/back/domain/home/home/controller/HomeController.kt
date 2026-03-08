7860fc6020197a6eda1fd495c9d9879619772ff6
lonelylonelylonely <lonelylonelylonely@example.com>
2026-03-08 16:44:48 +0900

    6

--- src/main/kotlin/com/back/domain/home/home/controller/HomeController.kt
+++ src/main/kotlin/com/back/domain/home/home/controller/HomeController.kt
@@ -1,12 +1,18 @@
 package com.back.domain.home.home.controller
 
+import com.back.domain.member.member.service.MemberService
 import jakarta.servlet.http.HttpSession
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class HomeController {
+class HomeController(
+    private val memberService: MemberService,
+) {
+    @GetMapping("/")
+    fun main() = "회원 수 : ${memberService.count()}"
+
     @GetMapping("/setSession")
     fun session(
         session: HttpSession,
